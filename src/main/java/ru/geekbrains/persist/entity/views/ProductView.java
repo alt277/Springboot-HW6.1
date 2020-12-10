47894970097e2c32578696901a0c3eec2891f6fa
alt277 <altima875@gmail.com>
2020-12-10 22:52:25 +0300

    ok

--- src/main/java/ru/geekbrains/persist/entity/views/ProductView.java
+++ src/main/java/ru/geekbrains/persist/entity/views/ProductView.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.persist.entity.views;
 
 public final class ProductView extends CommonView {
+
     public interface IdName extends CommonFull {}
     public interface FullProduct extends CommonFull {}
     public interface IdNamePrceCategory extends FullProduct,CategoryView.WholeCategory {}
